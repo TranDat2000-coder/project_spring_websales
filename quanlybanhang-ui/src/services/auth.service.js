913ca204f9b85ee60d7edf0b53dea36cf4decc4a
TranDat2000-coder <trandat.00712@gmail.com>
2023-06-26 21:09:56 +0700

    Update UI

--- quanlybanhang-ui/src/services/auth.service.js
+++ quanlybanhang-ui/src/services/auth.service.js
@@ -1,39 +1,43 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8081";
+const API_URL = "http://localhost:8080/api/auth/";
 
-class AuthService {
-  login(username, password) {
-    debugger;
-    return axios
-      .post(API_URL + "/login", {
-        username,
-        password
-      })
-      .then(response => {
-        if (response.data.token) {
-          localStorage.setItem("user", JSON.stringify(response.data));
-        }
+const register = (username, email, password) => {
+  return axios.post(API_URL + "signup", {
+    username,
+    email,
+    password,
+  });
+};
 
-        return response;
-      });
-  }
+const login = (username, password) => {
+  return axios
+    .post(API_URL + "signin", {
+      username,
+      password,
+    })
+    .then((response) => {
+      if (response.data.accessToken) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+      }
 
-  // logout() {
-  //   localStorage.removeItem("user");
-  // }
+      return response.data;
+    });
+};
 
-  // register(username, email, password) {
-  //   return axios.post(API_URL + "signup", {
-  //     username,
-  //     email,
-  //     password
-  //   });
-  // }
+const logout = () => {
+  localStorage.removeItem("user");
+};
 
-  getCurrentUser() {
-    return JSON.parse(localStorage.getItem('user'));;
-  }
-}
+const getCurrentUser = () => {
+  return JSON.parse(localStorage.getItem("user"));
+};
 
-export default new AuthService();
+const AuthService = {
+  register,
+  login,
+  logout,
+  getCurrentUser,
+};
+
+export default AuthService;
