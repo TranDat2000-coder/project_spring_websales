1b3d342ab0b4edc772bf1071f66c2c6a136f556a
TranDat2000-coder <trandat.00712@gmail.com>
2024-02-06 11:52:08 +0700

    Update auth request

--- quanlybanhang-service/src/main/java/com/project/quanlybanhang/request/AuthRequest.java
+++ quanlybanhang-service/src/main/java/com/project/quanlybanhang/request/AuthRequest.java
@@ -2,8 +2,14 @@ package com.project.quanlybanhang.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class AuthRequest {
+
+    @NotNull(message = "username is mandatory")
     private String username;
+
+    @NotNull(message = "password is mandatory")
     private String password;
 }
